425c87592ef23c2e9bf4548cbe3e404d21104eb6
ligang <ligangag@yonyou.com>
2021-07-28 12:59:49 +0800

    use api-gateway to get video_order with nacos

--- xdclass-api-gateway/src/main/java/net/gaven/ApiGatewayApplication.java
+++ xdclass-api-gateway/src/main/java/net/gaven/ApiGatewayApplication.java
@@ -2,12 +2,14 @@ package net.gaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author: lee
  * @create: 2021/7/28 10:53 上午
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
