07a11667c32182beafbe99deab3798625b72a26b
ligang <ligangag@yonyou.com>
2021-07-28 11:26:44 +0800

    use api-gateway to get video_order

---
+++ xdclass-api-gateway/src/main/java/net/gaven/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package net.gaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: lee
+ * @create: 2021/7/28 10:53 上午
+ **/
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
